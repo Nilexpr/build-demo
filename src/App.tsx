4bb304e8c29321470d2695e9f038f1dcdd50c78b
Your Name <you@example.com>
2025-06-14 11:08:31 +0000

    feat: init with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { createSignal } from "solid-js";
+
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <>
+      <div></div>
+      <h1>Vite + Solid</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p p-2>Click on the Vite and Solid logos to learn more</p>
+      <button
+      // bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+      // text="sm white"
+      // font="mono light"
+      // p="y-2 x-4"
+      // border="2 rounded blue-200"
+      >
+        Button
+      </button>
+      <button class="bg-blue-400 hover:bg-blue-500 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200 dark:bg-blue-500 dark:hover:bg-blue-600">
+        Button
+      </button>
+    </>
+  );
+}
+
+export default App;
